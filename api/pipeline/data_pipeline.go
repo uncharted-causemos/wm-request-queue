48837c11a2a31eb3d57388f97d49de87a2a93e6c
Venkat Korapaty <vkorapaty@admins-MacBook-Pro.local>
2021-09-29 18:09:59 -0400

    added comments

--- api/pipeline/data_pipeline.go
+++ api/pipeline/data_pipeline.go
@@ -90,6 +90,7 @@ func (d *DataPipelineRunner) Start() {
 	}()
 }
 
+// Submits the next item in the queue
 func (d *DataPipelineRunner) Submit() {
 	data, err := d.queue.Dequeue()
 	if err != nil {
--- api/routes/force_dispatch.go
+++ api/routes/force_dispatch.go
@@ -8,6 +8,8 @@ import (
 	"gitlab.uncharted.software/WM/wm-request-queue/config"
 )
 
+// Submits the next item in the queue regardless of prefect's busy status
+// or whether or not the data pipeline is running
 func ForceDispatchRequest(cfg *config.Config, requestQueue queue.RequestQueue, runner *pipeline.DataPipelineRunner) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		runner.Submit()
