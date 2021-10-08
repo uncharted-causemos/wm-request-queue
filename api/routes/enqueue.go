64e22619b0315821968aadc5d35401560a609a66
Venkat Korapaty <vkorapaty@admins-MacBook-Pro.local>
2021-10-08 18:38:25 -0400

    refactored enqueueing APIs with helper functions

--- api/routes/enqueue.go
+++ api/routes/enqueue.go
@@ -6,11 +6,10 @@ import (
 	"net/http"
 
 	"github.com/pkg/errors"
+	"gitlab.uncharted.software/WM/wm-request-queue/api/helpers"
 	"gitlab.uncharted.software/WM/wm-request-queue/api/pipeline"
 	"gitlab.uncharted.software/WM/wm-request-queue/api/queue"
 	"gitlab.uncharted.software/WM/wm-request-queue/config"
-
-	"github.com/vova616/xxhash"
 )
 
 // EnqueueRequest adds a request to the queue if there is space, or returns an error if
@@ -30,56 +29,25 @@ func EnqueueRequest(cfg *config.Config, requestQueue queue.RequestQueue) func(ht
 		// Decode and respond with a 400 on failure
 		err = json.Unmarshal(body, &enqueueMsg)
 		if err != nil {
-			handleErrorType(w, errors.Wrap(err, "failed to unmarshal enqueue request body"), http.StatusBadRequest, cfg.Logger)
+			handleErrorType(w, errors.Wrap(err, "failed to read enqueue request body"), http.StatusBadRequest, cfg.Logger)
 			return
 		}
+
 		// Store the full request body for forwarding to prefect
 		enqueueMsg.RequestData = body
 
-		// Make sure we've got valid data for the minimum field set
-		if enqueueMsg.ModelID == "" {
-			handleErrorType(w, errors.New("model_id missing"), http.StatusBadRequest, cfg.Logger)
-		}
-		if enqueueMsg.RunID == "" {
-			handleErrorType(w, errors.New("run_id_missing"), http.StatusBadRequest, cfg.Logger)
-		}
-		if len(enqueueMsg.DataPaths) == 0 {
-			handleErrorType(w, errors.New("data_paths missing"), http.StatusBadRequest, cfg.Logger)
-		}
-		for _, path := range enqueueMsg.DataPaths {
-			if path == "" {
-				handleErrorType(w, errors.New("data_paths missing"), http.StatusBadRequest, cfg.Logger)
-			}
+		err = helpers.CheckEnqueueParams(enqueueMsg)
+		if err != nil {
+			handleErrorType(w, err, http.StatusBadRequest, cfg.Logger)
 		}
 
-		// Create a hash from the request data
-		paramHash := xxhash.Checksum32(enqueueMsg.RequestData)
-
-		// Relevant info to run the request downstream
-		keyed := pipeline.KeyedEnqueueRequestData{
-			EnqueueRequestData: enqueueMsg,
-			RequestKey:         int32(paramHash),
-		}
-
-		// Enqueue the request if there's room, otherwise let the caller know that the service
-		// is unavailable.
-		if config.UseQueueIdempotency(cfg.Environment.DataPipelineIdempotencyChecks) {
-			result, err := requestQueue.EnqueueHashed(int(keyed.RequestKey), keyed)
-			if err != nil {
-				handleErrorType(w, err, http.StatusInternalServerError, cfg.Logger)
-			} else if !result {
-				handleErrorType(w, errors.New("request queue full"), http.StatusServiceUnavailable, cfg.Logger)
-				return
-			}
-		} else {
-			result, err := requestQueue.Enqueue(keyed)
-			if err != nil {
-				handleErrorType(w, err, http.StatusInternalServerError, cfg.Logger)
-				return
-			} else if !result {
-				handleErrorType(w, errors.New("request queue full"), http.StatusServiceUnavailable, cfg.Logger)
-				return
-			}
+		result, err := helpers.AddToQueue(enqueueMsg, *cfg, requestQueue)
+		if err != nil {
+			handleErrorType(w, err, http.StatusInternalServerError, cfg.Logger)
+			return
+		} else if !result {
+			handleErrorType(w, err, http.StatusServiceUnavailable, cfg.Logger)
+			return
 		}
 	}
 }
