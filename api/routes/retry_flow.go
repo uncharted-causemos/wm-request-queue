69f9231ae54ee66e264e51b6b517c6aead493ac2
Venkat Korapaty <vkorapaty@admins-MBP.home>
2021-11-11 09:31:34 -0800

    added retry flow api

---
+++ api/routes/retry_flow.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"net/http"
+
+	"gitlab.uncharted.software/WM/wm-request-queue/api/pipeline"
+	"gitlab.uncharted.software/WM/wm-request-queue/api/queue"
+	"gitlab.uncharted.software/WM/wm-request-queue/config"
+)
+
+// ForceDispatchRequest submits the next item in the queue regardless of prefect's busy status
+// or whether or not the data pipeline is running
+func RetryFlowRequest(cfg *config.Config, requestQueue queue.RequestQueue, runner *pipeline.DataPipelineRunner) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// path := strings.Split(r.URL.Path, "/")
+		// flow_id := path[len(path)-1]
+		runner.Submit()
+	}
+}
