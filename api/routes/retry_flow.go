02bd39dbdbdae3364b90b6594ee0c0923d589170
vkorapaty <vkorapaty@uncharted.software>
2021-12-23 11:04:56 -0500

    retrying uses new params

--- api/routes/retry_flow.go
+++ api/routes/retry_flow.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"strings"
 
@@ -17,6 +18,19 @@ func RetryFlowRequest(cfg *config.Config, requestQueue queue.RequestQueue, runne
 	return func(w http.ResponseWriter, r *http.Request) {
 		path := strings.Split(r.URL.Path, "/")
 		flowRunID := path[len(path)-1]
+		var enqueueParams pipeline.EnqueueRequestData
+
+		body, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			handleErrorType(w, errors.Wrap(err, "failed to read enqueue request body"), http.StatusBadRequest, cfg.Logger)
+			return
+		}
+		err = json.Unmarshal(body, &enqueueParams)
+		if err != nil {
+			handleErrorType(w, errors.Wrap(err, "failed to unmarshal request body"), http.StatusBadRequest, cfg.Logger)
+			return
+		}
 
 		if !runner.IsFlowDone(flowRunID) {
 			handleErrorType(w, errors.New("flow has not finished yet"), http.StatusBadRequest, cfg.Logger)
@@ -26,13 +40,29 @@ func RetryFlowRequest(cfg *config.Config, requestQueue queue.RequestQueue, runne
 		requestData := runner.RetrieveByFlowRunID(flowRunID)
 
 		var enqueueMsg pipeline.EnqueueRequestData
-		err := json.Unmarshal(requestData, &enqueueMsg)
+		err = json.Unmarshal(requestData, &enqueueMsg)
 		if err != nil {
 			handleErrorType(w, errors.Wrap(err, "failed to unmarshal request body"), http.StatusBadRequest, cfg.Logger)
 			return
 		}
 		// Store the full request body for forwarding to prefect
 		enqueueMsg.RequestData = requestData
+
+		if enqueueParams.ModelID != "" {
+			enqueueMsg.ModelID = enqueueParams.ModelID
+		}
+		if enqueueParams.RunID != "" {
+			enqueueMsg.RunID = enqueueParams.RunID
+		}
+		if enqueueParams.IsIndicator != enqueueMsg.IsIndicator {
+			enqueueMsg.IsIndicator = enqueueParams.IsIndicator
+		}
+		if len(enqueueParams.DocIDs) == 0 {
+			enqueueMsg.DocIDs = enqueueParams.DocIDs
+		}
+		if len(enqueueParams.DataPaths) == 0 {
+			enqueueMsg.DataPaths = enqueueParams.DataPaths
+		}
 
 		err = helpers.CheckEnqueueParams(enqueueMsg)
 		if err != nil {
